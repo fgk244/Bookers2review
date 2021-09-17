f01edbbc0dde3ea28f7a37e6e89a86c9008de502
fgk244 <fgk.244@gmail.com>
2021-09-17 10:18:16 +0000

    17commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+
+  get "/home/about" => "homes#about"
+  post 'books' => 'books#create'
+  resources :users
+  resources :books
+
+  resources :users, only: [:index, :show, :edit, :update]
+
+
 end
